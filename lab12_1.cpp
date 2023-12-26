84a661e477cd188abc7e96787f17dc342406c20f
AmericanoHoney <cocoart033@gmail.com>
2023-12-26 15:38:47 +0700

    complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int fibonacci (int);
 
 int main(){
     int x;
@@ -7,3 +8,14 @@ int main(){
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci (int x)
+{
+    if (x==1)
+        return 1;
+    else if (x==0)
+        return 0;
+    else
+        return fibonacci(x-1)+fibonacci(x-2);
+}
+
